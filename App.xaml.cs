c5dc2d7e73c73aa9e47ba2b7a142239af62175e5
Oleksandr Shevchuk <oleksandr.shevchuk222@gmail.com>
2024-05-21 22:45:44 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CryptoInfoApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
