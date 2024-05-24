8a7a8aa183894355b773aeaeb2f9501840d78f11
Oleksandr Shevchuk <oleksandr.shevchuk222@gmail.com>
2024-05-24 20:00:02 +0300

    Refactoring pages: displaying prices and page scrolling

---
+++ Helpers/Converters/MarketCapConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CryptoInfoApp.Helpers.Converters
+{
+    public class MarketCapConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Int64 price)
+            {
+                return price.ToString("C0", new CultureInfo("en-US"));
+            }
+            return value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Helpers/Converters/PriceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CryptoInfoApp.Helpers.Converters
+{
+    public class PriceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+            if (value is decimal price)
+            {
+                string formattedPrice = price.ToString("#,0.####", CultureInfo.InvariantCulture);
+                return "$" + formattedPrice;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
