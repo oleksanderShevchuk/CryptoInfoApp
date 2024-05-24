a2018e8e113b5736a712976e5ae9e0faf7cc60ff
Oleksandr Shevchuk <oleksandr.shevchuk222@gmail.com>
2024-05-24 13:50:47 +0300

    Added app theme change MainWindow refactoring

---
+++ Helpers/AppTheme.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace CryptoInfoApp.Helpers
+{
+    public class AppTheme
+    {
+        public static void ChangeTheme(Uri themeUri)
+        {
+            ResourceDictionary theme = new ResourceDictionary()
+            {
+                Source = themeUri,
+            };
+            App.Current.Resources.Clear();
+            App.Current.Resources.MergedDictionaries.Add(theme);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CryptoInfoApp.Helpers;
+using CryptoInfoApp.Services;
+using CryptoInfoApp.ViewModels;
+using CryptoInfoApp.Views;
+using System.Windows;
+
+namespace CryptoInfoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var cryptoService = new CoinGeckoService();
+            DataContext = new MainViewModel(cryptoService);
+
+            MainView.Navigate(new MainView { DataContext = DataContext });
+        }
+        private void LightThemeClick(object sender, RoutedEventArgs e)
+        {
+            AppTheme.ChangeTheme(new Uri($"Themes/Light.xaml", UriKind.Relative));
+        }
+        private void DarkThemeClick(object sender, RoutedEventArgs e)
+        {
+            AppTheme.ChangeTheme(new Uri($"Themes/Dark.xaml", UriKind.Relative));
+        }
+    }
+}
